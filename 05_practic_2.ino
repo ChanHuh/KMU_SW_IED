ede0f60c11961b75a475154e476220b34dfb46ed
ChanHuh <66400428+ChanHuh@users.noreply.github.com>
2020-09-21 12:01:54 +0900

    Create 05_practic_2.ino

---
+++ 05_practic_2.ino
@@ -0,0 +1,38 @@
+int red = 12;  
+void setup() {  
+  // put your setup code here, to run once:  
+  pinMode(red, OUTPUT);
+  digitalWrite(red, HIGH);  
+  delay(1000);
+  digitalWrite(red, LOW);
+  delay(100);
+  digitalWrite(red, HIGH);
+  delay(100);
+
+  digitalWrite(red, LOW);
+  delay(100);
+  digitalWrite(red, HIGH);
+  delay(100);
+
+  digitalWrite(red, LOW);
+  delay(100);
+  digitalWrite(red, HIGH);
+  delay(100);
+
+  digitalWrite(red, LOW);
+  delay(100);
+  digitalWrite(red, HIGH);
+  delay(100);
+
+  digitalWrite(red, LOW);
+  delay(100);
+  digitalWrite(red, HIGH);
+  delay(100);
+  digitalWrite(red, LOW);
+}
+     
+ 
+   
+void loop() {  
+
+}
